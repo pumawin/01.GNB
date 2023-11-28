4f13906a56fc48f89d3a2bdec6e21c63e929b0e9
windy <kjyy2028@gmail.com>
2023-11-28 13:17:23 +0900

    콘솔정리

--- 01/js/script.js
+++ 01/js/script.js
@@ -5,6 +5,8 @@ $(function () {
   const $submenu = $('.submenu');
   const duration = 300;
 
+  // console.log($menu);
+
   // 메뉴 영역에 마우스가 들어왔을 때
   $menu.on('mouseenter', function () {
     // 해당 메뉴의 서브메뉴를 slide down
